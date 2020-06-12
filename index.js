d122ef7576da287c32bde7a64316fc8bbd341031
yaevvv <yaevvv@gmail.com>
2020-06-12 13:03:36 +0300

    Main project

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // index.js
-require('./app/index')
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'John'
+    })
+})
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.listen(3000)
